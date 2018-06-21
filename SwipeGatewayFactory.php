dac2a1f13b60000acfa68c66ce849d40e9777cce
dmitry <dch@digitalais.lv>
2018-06-21 17:02:32 +0300

    hotfix for symfony container

--- SwipeGatewayFactory.php
+++ SwipeGatewayFactory.php
@@ -28,7 +28,7 @@ class SwipeGatewayFactory extends GatewayFactory
             'payum.action.cancel' => new CancelAction(),
 //            'payum.action.notify' => new NotifyAction(),
             'payum.action.status' => new StatusAction(),
-            'payum.action.convert_payment' => new ConvertPaymentAction(),
+//            'payum.action.convert_payment' => new ConvertPaymentAction(),
         ]);
         $config['payum.default_options'] = array(
             'sandbox' => false,
